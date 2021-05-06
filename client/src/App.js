cfdf35b91a022ae3dbcc56af9de945c889cd0503
Carlos Alonso <carlos.alonso@CPM15124.local>
2021-05-06 10:38:33 +0200

    Testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Fib Calculator</h1>
+          <p>Does it work?</p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
